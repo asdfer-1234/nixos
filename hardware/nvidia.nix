74a29139ae47e7880a1ea30734f51d3afce1b07a
asdfer <>
2026-01-18 19:49:04 +0900

    Update nvidia.nix

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,25 +1,30 @@
 { config, lib, ... }:
 {
-  system.nixos.tags = [ "dGPU only" ];
 
-  hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [
-    "modesetting"
-    "nvidia"
-  ];
-  hardware.nvidia.open = true;
+  config =
+    lib.mkMerge
+      {
+        hardware.graphics.enable = true;
+        services.xserver.videoDrivers = [
+          "modesetting"
+          "nvidia"
+        ];
+        hardware.nvidia.open = true;
 
-  # Intel: 0000:00:02:0
-  # NVIDIA: 0000:01:00:0
-  hardware.nvidia.prime = {
-    intelBusId = "PCI:0@0:2:0";
-    nvidiaBusId = "PCI:1@0:0:0";
-    offload.enable = true;
-  };
-
-  config = lib.mkIf (config.specialisation != { }) {
-    boot.kernelParams = [ "module_blacklist=i915" ];
-  };
+        # Intel: 0000:00:02:0
+        # NVIDIA: 0000:01:00:0
+        hardware.nvidia.prime = {
+          intelBusId = "PCI:0@0:2:0";
+          nvidiaBusId = "PCI:1@0:0:0";
+          offload.enable = true;
+        };
+      }
+      (
+        lib.mkIf (config.specialisation != { }) {
+          system.nixos.tags = [ "dGPU only" ];
+          boot.kernelParams = [ "module_blacklist=i915" ];
+        }
+      );
 
   specialisation = {
     offload.configuration = {
