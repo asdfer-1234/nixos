bd8ac605bf93c3e5a8d0f6eaa54a9b253cfdcbdc
asdfer <>
2026-01-18 19:13:49 +0900

    asdf

--- flake.nix
+++ flake.nix
@@ -17,12 +17,7 @@
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.asdfer = ./users/asdfer/home.nix;
-            home-manager.users.zxcver = ./users/zxcver/home.nix;
-          }
+          ./home-manager.nix
         ];
       };
     };
