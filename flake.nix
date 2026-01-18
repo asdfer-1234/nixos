fbf7a8e460849dc1410e67c7fb854693020d3b67
asdfer <>
2026-01-18 19:19:21 +0900

    asdf

--- flake.nix
+++ flake.nix
@@ -13,9 +13,10 @@
     }@inputs:
     {
       nixosConfigurations.asdfhost = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs home-manager; };
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager
           ./home-manager.nix
         ];
       };
