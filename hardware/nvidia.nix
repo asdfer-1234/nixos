80bde9f010152c0deef27b53e9836233d0faf166
asdfer <>
2026-01-18 19:51:19 +0900

    asdf

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,37 +1,37 @@
 { config, lib, ... }:
 {
 
-  config =
-    lib.mkMerge
-      {
-        hardware.graphics.enable = true;
-        services.xserver.videoDrivers = [
-          "modesetting"
-          "nvidia"
-        ];
-        hardware.nvidia.open = true;
+  config = lib.mkMerge [
+    {
+      hardware.graphics.enable = true;
+      services.xserver.videoDrivers = [
+        "modesetting"
+        "nvidia"
+      ];
+      hardware.nvidia.open = true;
 
-        # Intel: 0000:00:02:0
-        # NVIDIA: 0000:01:00:0
-        hardware.nvidia.prime = {
-          intelBusId = "PCI:0@0:2:0";
-          nvidiaBusId = "PCI:1@0:0:0";
-          offload.enable = true;
-        };
-      }
-      (lib.mkIf (config.specialisation != { }) {
-        system.nixos.tags = [ "dGPU only" ];
-        boot.kernelParams = [ "module_blacklist=i915" ];
-      })
-      {
-        specialisation = {
-          offload.configuration = {
-            system.nixos.tags = [ "iGPU with offload" ];
-            hardware.nvidia.prime = {
-              offload.enable = lib.mkForce true;
-            };
+      # Intel: 0000:00:02:0
+      # NVIDIA: 0000:01:00:0
+      hardware.nvidia.prime = {
+        intelBusId = "PCI:0@0:2:0";
+        nvidiaBusId = "PCI:1@0:0:0";
+        offload.enable = true;
+      };
+    }
+    (lib.mkIf (config.specialisation != { }) {
+      system.nixos.tags = [ "dGPU only" ];
+      boot.kernelParams = [ "module_blacklist=i915" ];
+    })
+    {
+      specialisation = {
+        offload.configuration = {
+          system.nixos.tags = [ "iGPU with offload" ];
+          hardware.nvidia.prime = {
+            offload.enable = lib.mkForce true;
           };
         };
       };
+    }
+  ];
 
 }
