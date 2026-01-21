9b9d863ad57cae17dd2513f3d48c7e5879ca78f6
asdfer <>
2026-01-21 17:07:01 +0900

    asdf

--- asdfrice/rice.nix
+++ asdfrice/rice.nix
@@ -11,18 +11,9 @@ in
 
   imports = [ ../myModules/kanata.nix ];
   options.rice.asdfrice = {
-    enable = lib.mkEnableOption "yay";
+    enable = lib.mkEnableOption "yay!";
   };
 
   config = lib.mkIf cfg.enable {
-    xdg.configFile = {
-      "niri/" = {
-        source = ./dotfile;
-        force = true;
-      };
-    };
-    home.packages = [
-      pkgs.niri
-    ];
   };
 }
