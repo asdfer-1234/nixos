dadde18f1d4f84bd885d79d0f097b2d11e269c0d
asdfer <>
2026-01-21 17:05:39 +0900

    asdf

--- myModules/niri.nix
+++ myModules/niri.nix
@@ -5,14 +5,18 @@
   ...
 }:
 let
-  cfg = config.asdfRice.niri;
+  cfg = config.myModules.niri;
 in
 {
-  imports = [ .././kanata/kanata.nix ];
+  options.myModules.niri = {
+    enable = lib.mkEnableOption;
+    config = lib.mkOption { type = lib.types.str; };
+  };
+
   config = lib.mkIf cfg.enable {
     xdg.configFile = {
       "niri/" = {
-        source = ./dotfile;
+        text = cfg.config;
         force = true;
       };
     };
