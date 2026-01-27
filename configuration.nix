f91c974e497ee8dde6bfbe49c50199aef57a65e3
asdfer <>
2026-01-27 14:51:11 +0900

    asdf

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModprobeConfig = ''
     options iwlwifi 11n_disable=8
+
   '';
 
   networking.hostName = "asdfhost"; # Define your hostname.
