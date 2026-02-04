e1c5df16e7aad2b49f0c2bed9430d175d8f4d541
asdfer <>
2026-02-04 16:33:54 +0900

    asdf

--- configuration.nix
+++ configuration.nix
@@ -62,6 +62,18 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+
   environment.systemPackages = [
     pkgs.vim
     pkgs.neovim
