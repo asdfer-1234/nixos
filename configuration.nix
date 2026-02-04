179ed6f9234261b7cec3b9b96e37ad8c0ec9f991
asdfer <>
2026-02-04 15:06:25 +0900

    asdf

--- configuration.nix
+++ configuration.nix
@@ -95,4 +95,15 @@
     extraSpecialArgs = { inherit inputs qsrs; };
   };
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-gtk
+      ];
+      waylandFrontend = true;
+    };
+  };
+
 }
