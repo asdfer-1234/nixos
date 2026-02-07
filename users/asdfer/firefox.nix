dd4424409dec0c5d35a5b62c9ccc95082460dafa
asdfer <>
2026-02-08 00:37:25 +0900

    asdf

--- users/asdfer/firefox.nix
+++ users/asdfer/firefox.nix
@@ -16,6 +16,7 @@ with lib;
                 template = "https://namu.wiki/w/{searchTerms}";
               }
             ];
+            definedAliases = [ "@namu" ];
           };
 
           "Nix Packages" = {
