79d611fd61164313d28d17d6c7211d91a1fe3231
asdfer <>
2026-02-08 01:16:31 +0900

    asdf

--- flake.nix
+++ flake.nix
@@ -22,7 +22,9 @@
       nixosConfigurations.nauvis = nixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit inputs qsrs;
-          ml.importGen = imports: forEach imports (p: (if (pathIsDirectory p) then p else (p + ".nix")));
+          ml.importGen =
+            imports:
+            forEach imports (p: (if ((pathIsDirectory p) || (match p ''\.nix$'')) then p else (p + ".nix")));
         };
         modules = [
           ./configuration.nix
