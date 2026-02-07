fb0d2dd5f09bd4c9e5677aa58c4947b4fb0509ff
asdfer <>
2026-02-08 00:27:07 +0900

    asdf

--- users/asdfer/firefox.nix
+++ users/asdfer/firefox.nix
@@ -16,7 +16,7 @@ with lib;
               params = [
                 {
                   name = "channel";
-                  value = "unstable";
+                  value = "25.11";
                 }
                 {
                   name = "query";
@@ -63,6 +63,43 @@ with lib;
           ];
           icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
           definedAliases = [ "@nw" ];
+        };
+      };
+      # Extension configuration
+      "3rdparty".Extensions = {
+        "uBlock0@raymondhill.net".adminSettings = {
+          userSettings = rec {
+            uiTheme = "dark";
+            uiAccentCustom = true;
+            uiAccentCustom0 = "#8300ff";
+            cloudStorageEnabled = mkForce false;
+
+            importedLists = [
+              "https:#filters.adtidy.org/extension/ublock/filters/3.txt"
+              "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+            ];
+
+            externalLists = lib.concatStringsSep "\n" importedLists;
+          };
+
+          selectedFilterLists = [
+            "CZE-0"
+            "adguard-generic"
+            "adguard-annoyance"
+            "adguard-social"
+            "adguard-spyware-url"
+            "easylist"
+            "easyprivacy"
+            "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+            "plowe-0"
+            "ublock-abuse"
+            "ublock-badware"
+            "ublock-filters"
+            "ublock-privacy"
+            "ublock-quick-fixes"
+            "ublock-unbreak"
+            "urlhaus-1"
+          ];
         };
       };
     };
