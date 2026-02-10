9f39adaa53ee62d976381edecdc255075198d6b4
asdfer <>
2026-02-10 10:25:54 +0900

    asdf

--- switch.bash
+++ switch.bash
@@ -6,7 +6,9 @@ git add .
 git commit -m "asdf"
 
 if [ -v NIXOS_SPECIALIZATION ]; then
+    echo asdf
     nixos-rebuild switch --sudo --specialisation=$NIXOS_SPECIALIZATION
 else
+    echo zxcv
     nixos-rebuild switch --sudo
 fi
