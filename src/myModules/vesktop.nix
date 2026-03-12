0c6bd04a0a4d513a12ec2c8a53267a2f9e0a1560
asdfer <>
2026-03-12 16:59:36 +0900

    asdf

---
+++ src/myModules/vesktop.nix
@@ -0,0 +1,28 @@
+{ lib, ... }:
+with lib;
+let
+  cfg = config.myModules.vesktop;
+in
+{
+  options.myModules.vesktop = {
+    enable = mkEnableOption "vesktop";
+  };
+
+  config = mkIf cfg.enable {
+    programs.vesktop = {
+      enable = true;
+      settings = {
+        discordBranch = "stable";
+      };
+    };
+    xdg.configFile = {
+      "vesktop/state.json" = {
+        text = ''
+          {
+            firstLaunch: false
+          }
+        '';
+      };
+    };
+  };
+}
