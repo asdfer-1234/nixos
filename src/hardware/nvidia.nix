2de08faffd5c36c0958c49ebbe8c33fc46922368
asdfer <>
2026-03-12 11:29:49 +0900

    asdf

--- src/hardware/nvidia.nix
+++ src/hardware/nvidia.nix
@@ -44,6 +44,7 @@
             softdep amdgpu pre: vfio vfio_pci
             softdep i915 pre: vfio vfio_pci
             softdep nvidia pre: vfio vfio_pci
+            softdep nvidia pre: i915
           '';
         };
       };
