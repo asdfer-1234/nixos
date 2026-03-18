f433231032fe22d903374d2ca0af64a2dc3a0b3f
asdfer <>
2026-03-18 23:51:22 +0900

    asdf

--- src/my/hardware/networking.nix
+++ src/my/hardware/networking.nix
@@ -1,8 +1,8 @@
 { lib, config, ... }:
 with lib;
 {
-  options.my.hardware.networking = mkEnableOption "";
-  config = mkIf config.my.hardware.networking {
+  options.my.hardware.networking.enable = mkEnableOption "";
+  config = mkIf config.my.hardware.networking.enable {
     networking.networkmanager.enable = true;
     boot.extraModprobeConfig = "options iwlwifi 11n_disable=8\n";
   };
