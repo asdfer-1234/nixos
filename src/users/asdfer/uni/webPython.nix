494f5da758f3eba6caa73b27c1acad2c962748a7
asdfer <>
2026-03-18 21:19:54 +0900

    asdf

--- src/users/asdfer/uni/webPython.nix
+++ src/users/asdfer/uni/webPython.nix
@@ -4,7 +4,7 @@ with lib;
   options = {
     myModules.uni.webPython.enable = mkEnableOption "";
   };
-  config = mkIf myModules.uni.webPython.enable {
+  config = mkIf config.myModules.uni.webPython.enable {
     home.packages = with pkgs; [
       python3
       conda
