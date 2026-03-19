1666690a36fb371c1bc5286e477eebca0ab5c1a1
asdfer <>
2026-03-19 12:51:05 +0900

    asdf

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
         mkEnableModule =
           config: attrPath: value:
           let
-            attrPathStringList = (path.subpath.components (path.splitRoot attrPath)).subpath;
+            attrPathStringList = path.subpath.components (path.splitRoot attrPath).subpath;
           in
           {
             options = setAttrByPath attrPathStringList {
