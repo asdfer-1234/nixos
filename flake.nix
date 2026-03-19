cb1b404bd194cb7a2d136c6c8e286fb0564254d4
asdfer <>
2026-03-19 12:38:25 +0900

    asdf

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           options = setAttrByPath attrPath {
             enable = mkEnableOption "an enable option blah";
           };
-          config = mkIf (attrByPath (attrPath ++ [ enable ])) value;
+          config = mkIf (attrByPath ([ "config" ] ++ attrPath ++ [ enable ]) == null) value;
         };
       };
     in
