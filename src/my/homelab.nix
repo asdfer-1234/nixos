c6926f54eb92969f3aac1c9e8b9d1cb3d61d1f2b
asdfer <>
2026-03-19 17:07:55 +0900

    asdf

--- src/my/homelab.nix
+++ src/my/homelab.nix
@@ -9,15 +9,9 @@ mkEnableModule config /my/homelab {
   services.vaultwarden = {
     enable = true;
 
-    domain = "https://localhost";
+    domain = "localhost";
 
     config = {
-      DOMAIN = "https://localhost";
-      # Vaultwarden recommends running behind a reverse proxy, the configureNginx option can be used for that.
-      ROCKET_ADDRESS = "127.0.0.1";
-      ROCKET_PORT = 8222;
-
-      ROCKET_LOG = "critical";
     };
   };
 }
