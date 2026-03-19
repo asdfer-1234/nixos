ebed0ece7241e628e1aa5ed7ba8106648eeb8bcf
asdfer <>
2026-03-19 17:02:54 +0900

    asdf

--- src/my/homelab.nix
+++ src/my/homelab.nix
@@ -6,5 +6,11 @@
 }:
 with myLib;
 mkEnableModule config /my/homelab {
-  services.vaultwarden.enable = true;
+  services.vaultwarden = {
+    enable = true;
+
+    config = {
+      DOMAIN = "https://localhost";
+    };
+  };
 }
