1278d5e54ffbb125044faf1ccaa13716bdafb6e0
asdfer <>
2026-03-19 12:52:20 +0900

    asdf

--- src/users/asdfer/default.nix
+++ src/users/asdfer/default.nix
@@ -7,28 +7,25 @@
 }:
 with lib;
 with myLib;
-{
-  options.my.users.asdfer.enable = mkEnableOption "";
-  config = mkIf config.my.users.asdfer.enable {
-    users.users.asdfer = {
-      isNormalUser = true;
-      description = "the main user with sudo privileges";
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-        "uinput"
-        "input"
-      ];
-    };
-    home-manager.users.asdfer = nixPath ./home;
-    my.home-manager.enable = true;
-
-    programs.niri.enable = true; # is this really needed?
-    programs.steam = {
-      enable = true;
-      protontricks.enable = true;
-      extraPackages = with pkgs; [ protonup-qt ];
-    };
-    services.logmein-hamachi.enable = true;
+mkEnableOption config /my/users/asdfer {
+  users.users.asdfer = {
+    isNormalUser = true;
+    description = "the main user with sudo privileges";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "uinput"
+      "input"
+    ];
   };
+  home-manager.users.asdfer = nixPath ./home;
+  my.home-manager.enable = true;
+
+  programs.niri.enable = true; # is this really needed?
+  programs.steam = {
+    enable = true;
+    protontricks.enable = true;
+    extraPackages = with pkgs; [ protonup-qt ];
+  };
+  services.logmein-hamachi.enable = true;
 }
