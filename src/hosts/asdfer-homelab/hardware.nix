32c25c05a5f0e156c5913ea167be45251028648d
asdfer <>
2026-03-25 00:34:48 +0900

    asdf

--- src/hosts/asdfer-homelab/hardware.nix
+++ src/hosts/asdfer-homelab/hardware.nix
@@ -6,7 +6,13 @@
 with lib;
 {
   hardware.enableRedistributableFirmware = true;
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ata_piix" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ehci_pci"
+    "ata_piix"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModprobeConfig = "softdep i915 pre: vfio vfio_pci";
 
@@ -31,6 +37,9 @@ with lib;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  services.udev.extraRules = ''
+    ENV{ID_PATH}=="pci-0000:00:1f.2-ata-1.0", ENV{UDISKS_PRESENTATION_HIDE}="1"
+  '';
 
   # Disable nvidia including nouveau
   boot.blacklistedKernelModules = [
