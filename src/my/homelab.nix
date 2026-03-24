1bca19cf62a10392473fd3024050a84f04be9fe0
asdfer <>
2026-03-25 08:14:06 +0900

    asdf

--- src/my/homelab.nix
+++ src/my/homelab.nix
@@ -38,6 +38,7 @@ mkEnableModule config /my/homelab {
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
       AllowUsers = [ "access" ];
+      GatewayPorts = "yes";
     };
   };
 
@@ -50,6 +51,7 @@ mkEnableModule config /my/homelab {
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM88TBQcNoOEGSnx7CoGnrt/BGFVXdiJjLaMRZxuqFXB asdfer"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2p/d5bq0GCaLdgkxUG3rFVM4S6jddQDGdxX9XPZw00CQzbMAKpX9vRdyFeE8PMVxzrGROsIgZQTN2PQTcifJLNdWfz9gLnB6zNsljZXSrgINF8OZ0HhWxbbV0vrfnugcYiBo+8AIur/pbgcMJ6JaP8y6SgNjn1dQqmQsDyJDZF8KDsnUqUZD6gSd9tzw0ZNMAgnp2YQ2WfrHuEUnKLUlNfniRda4GuT9OQlB8ngah6wfuAXcHvOmwK7QhsAJhw6j00w2mlI1bV5m1qCR40TJoLYjsoCAvm4HIoetXXZpcCjcai4D2XwSDlheibr9dMY1ojGatFbp3ZH1BtpFQXN8n a"
     ];
   };
 }
