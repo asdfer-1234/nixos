f36e28c35941d87b4d2045f7cdecb76ec74e5ef8
asdfer <>
2026-03-26 11:39:21 +0900

    asdf

--- src/my/hardware/networking.nix
+++ src/my/hardware/networking.nix
@@ -10,8 +10,35 @@ mkEnableModule config /my/hardware/networking {
   boot.extraModprobeConfig = "options iwlwifi 11n_disable=8\n";
   networking.networkmanager = {
     enable = true;
-    ensureProfiles.profiles = {
-
-    };
+    ensureProfiles.profiles =
+      let
+        mkSimpleProfile =
+          {
+            ssid,
+            id ? ssid,
+            pw,
+          }:
+          {
+            connection = {
+              id = ssid;
+              interface-name = "wlp46s0";
+              type = "wifi";
+            };
+            wifi = {
+              inherit ssid;
+            };
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-psk";
+              psk = pw;
+            };
+          };
+      in
+      {
+        asdfer-hotspot = mkSimpleProfile {
+          ssid = "asdfer-hotspot";
+          pw = "asdfasdfasdf";
+        };
+      };
   };
 }
